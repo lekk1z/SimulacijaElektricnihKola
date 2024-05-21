1112e4ff46d255e8d238e2eea6f135c7b0126aa3
lekk1z <2021.09.02@online.devetagimnazija.edu.rs>
2024-05-21 18:21:54 +0200

    Uvodjenje naizmenicne struje
    Dodat inteface za sve komponente naizmenicne struje i klasa za termogeni otpornik

---
+++ SimulacijaElektricnihKola/SimulacijaElektricnihKola/Naizmenicna Struja/TermogenaOtpornost.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SimulacijaElektricnihKola.Naizmenicna_Struja
+{
+    internal class TermogenaOtpornost<TMaterijal> : Otpornik<TMaterijal>, INaizmenicnaStruja where TMaterijal : IMaterijali
+    {
+        public double naponAmplituda { get; }
+       public  double naponEfektivna { get { return naponAmplituda / Math.Sqrt(2); } }
+
+        public double strujaAmplituda { get; }
+        public double strujaEfektivna { get { return strujaAmplituda / Math.Sqrt(2); } }
+        public double frekvencija { get; }
+        public double IzracunajEMS()
+        {
+            return naponEfektivna * strujaEfektivna;
+        }
+        public double IzracunajKolicinuToplote(TimeSpan t)
+        {
+            return IzracunajEMS()*t.TotalSeconds;
+        }
+
+        public TermogenaOtpornost(double napon, double struja, double frekvencija,double duzina,double poluprecnik, string ime):base(duzina,poluprecnik,ime)
+        {
+            this.naponAmplituda = napon;
+            this.strujaAmplituda = struja;
+            this.frekvencija = frekvencija;
+        }
+
+        
+    }
+}
