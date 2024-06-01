fb123f999bd87db0bfb3eef2ff899bb71398ba4e
AleksandraPopov <104566389+AleksandraPopov@users.noreply.github.com>
2024-06-01 23:36:02 +0200

    Namesteno i objasnjenje za naizmenicnu, fale samo slike

--- SimulacijaElektricnihKola/SimulacijaElektricnihKola/Teorija.cs
+++ SimulacijaElektricnihKola/SimulacijaElektricnihKola/Teorija.cs
@@ -39,24 +39,8 @@ namespace SimulacijaElektricnihKola
 		{
             this.Width = 1200;
             this.Height = 800;
-            rtbTeorija.SelectionAlignment = HorizontalAlignment.Center;
-            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("JEDNOSMERNA STRUJA");
-            rtbTeorija.SelectionLength = "JEDNOSMERNA STRUJA".Length;
-            rtbTeorija.SelectionFont = new Font(rtbTeorija.Font, FontStyle.Bold);
-            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("Primer:");
-            rtbTeorija.SelectionLength = "Primer:".Length;
-            rtbTeorija.SelectionFont = new Font(rtbTeorija.Font, FontStyle.Italic);
-            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("PRVI KIRHOFOV ZAKON");
-            rtbTeorija.SelectionLength = "PRVI KIRHOFOV ZAKON".Length;
-            rtbTeorija.SelectionFont = new Font(rtbTeorija.Font, FontStyle.Bold);
-            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("DRUGI KIRHOFOV ZAKON");
-            rtbTeorija.SelectionLength = "DRUGI KIRHOFOV ZAKON".Length;
-            rtbTeorija.SelectionFont = new Font(rtbTeorija.Font, FontStyle.Bold);
-            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("DRUGI KIRHOFOV ZAKON");
-            rtbTeorija.SelectionLength = "DRUGI KIRHOFOV ZAKON".Length;
-            rtbTeorija.SelectionFont = new Font(rtbTeorija.Font, FontStyle.Bold);
-
-			SetSize();
+          
+            SetSize();
 
 		}
 
@@ -67,9 +51,44 @@ namespace SimulacijaElektricnihKola
         public void SetSize()
         {
 			rtbTeorija.SetBounds( 0, 0, 9*Width/10, ClientSize.Height );
-            rtbTeorija.Font = new Font("", Width/70, FontStyle.Regular);
+            rtbTeorija.Font = new Font("", Width/70);
 			btnNazad.SetBounds( Width - Width/12, Height/50, Width/17, Width/17 );
 			btnNazad.Font = new Font( "", Width/50, FontStyle.Bold );
-		}
+            rtbTeorija.SelectionAlignment = HorizontalAlignment.Center;
+            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("JEDNOSMERNA STRUJA");
+            rtbTeorija.SelectionLength = "JEDNOSMERNA STRUJA".Length;
+             rtbTeorija.SelectionFont = new Font("",Width/50, FontStyle.Bold);
+            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("Primer:");
+            rtbTeorija.SelectionLength = "Primer:".Length;
+            rtbTeorija.SelectionFont = new Font(rtbTeorija.Font, FontStyle.Italic);
+            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("PRVI KIRHOFOV ZAKON");
+            rtbTeorija.SelectionLength = "PRVI KIRHOFOV ZAKON".Length;
+            rtbTeorija.SelectionFont = new Font("", Width / 50, FontStyle.Bold);
+            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("DRUGI KIRHOFOV ZAKON");
+            rtbTeorija.SelectionLength = "DRUGI KIRHOFOV ZAKON".Length;
+            rtbTeorija.SelectionFont = new Font("", Width / 50, FontStyle.Bold);
+            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("NAIZMENIČNA STRUJA");
+            rtbTeorija.SelectionLength = "NAIZMENIČNA STRUJA".Length;
+            rtbTeorija.SelectionFont = new Font("", Width / 50, FontStyle.Bold);
+            rtbTeorija.SelectionAlignment = HorizontalAlignment.Center;
+            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("Termogeni otpor (R)");
+            rtbTeorija.SelectionLength = "Termogeni otpor (R)".Length;
+            rtbTeorija.SelectionFont = new Font("", Width / 50, FontStyle.Bold);
+            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("Induktivni otpor (XL)");
+            rtbTeorija.SelectionLength = "Induktivni otpor (XL)".Length;
+            rtbTeorija.SelectionFont = new Font("", Width / 50, FontStyle.Bold);
+            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("Kapacitivni otpor (XC)");
+            rtbTeorija.SelectionLength = "Kapacitivni otpor (XC)".Length;
+            rtbTeorija.SelectionFont = new Font("", Width / 50, FontStyle.Bold);
+            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("Induktivna otpornost");
+            rtbTeorija.SelectionLength = "Induktivna otpornost".Length;
+            rtbTeorija.SelectionFont = new Font("", Width / 50, FontStyle.Bold);
+            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("Kapacitivna otpornost");
+            rtbTeorija.SelectionLength = "Kapacitivna otpornost".Length;
+            rtbTeorija.SelectionFont = new Font("", Width / 50, FontStyle.Bold);
+            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("Kapacitivna otpornost");
+            rtbTeorija.SelectionLength = "Kapacitivna otpornost".Length;
+            rtbTeorija.SelectionFont = new Font("", Width / 50, FontStyle.Bold);
+        }
     }
 }
