70d1f22faa772287724f6cfba050b664d86f60f9
Vukasin Jovanovic <2021.09.07@online.devetagimnazija.edu.rs>
2024-06-01 23:56:50 +0200

    Simulacija ali jos nije gotova

--- SimulacijaElektricnihKola/SimulacijaElektricnihKola/Simulacija.Designer.cs
+++ SimulacijaElektricnihKola/SimulacijaElektricnihKola/Simulacija.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+<<<<<<< Updated upstream
 			this.btnNazad = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
@@ -55,11 +56,90 @@
 			this.Load += new System.EventHandler(this.Simulacija_Load);
 			this.ResizeEnd += new System.EventHandler(this.Simulacija_ResizeEnd);
 			this.ResumeLayout(false);
+=======
+            this.components = new System.ComponentModel.Container();
+            this.btnNazad = new System.Windows.Forms.Button();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnNazad
+            // 
+            this.btnNazad.Location = new System.Drawing.Point(540, 11);
+            this.btnNazad.Name = "btnNazad";
+            this.btnNazad.Size = new System.Drawing.Size(50, 52);
+            this.btnNazad.TabIndex = 2;
+            this.btnNazad.Text = "<";
+            this.btnNazad.UseVisualStyleBackColor = true;
+            this.btnNazad.Click += new System.EventHandler(this.btnNazad_Click);
+            // 
+            // pb1
+            // 
+            this.pb1.Location = new System.Drawing.Point(34, 120);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(132, 127);
+            this.pb1.TabIndex = 4;
+            this.pb1.TabStop = false;
+            this.pb1.Click += new System.EventHandler(this.pb1_Click);
+            // 
+            // pb2
+            // 
+            this.pb2.Location = new System.Drawing.Point(237, 120);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(132, 127);
+            this.pb2.TabIndex = 5;
+            this.pb2.TabStop = false;
+            this.pb2.Click += new System.EventHandler(this.pb2_Click);
+            this.pb2.Paint += new System.Windows.Forms.PaintEventHandler(this.pb2_Paint);
+            // 
+            // pb3
+            // 
+            this.pb3.Location = new System.Drawing.Point(428, 120);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(132, 127);
+            this.pb3.TabIndex = 6;
+            this.pb3.TabStop = false;
+            this.pb3.Click += new System.EventHandler(this.pb3_Click);
+            this.pb3.Paint += new System.Windows.Forms.PaintEventHandler(this.pb3_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Simulacija
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.pb3);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.btnNazad);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Simulacija";
+            this.Text = "Simulacija";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Simulacija_FormClosed);
+            this.Load += new System.EventHandler(this.Simulacija_Load);
+            this.ResizeEnd += new System.EventHandler(this.Simulacija_ResizeEnd);
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            this.ResumeLayout(false);
+>>>>>>> Stashed changes
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnNazad;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- SimulacijaElektricnihKola/SimulacijaElektricnihKola/Simulacija.cs
+++ SimulacijaElektricnihKola/SimulacijaElektricnihKola/Simulacija.cs
@@ -12,6 +12,9 @@ namespace SimulacijaElektricnihKola
 {
     public partial class Simulacija : Form
     {
+        Pen olovka;
+       
+        static int brojac = 0;
         public Simulacija()
         {
             InitializeComponent();
@@ -44,5 +47,49 @@ namespace SimulacijaElektricnihKola
 			btnNazad.SetBounds( Width - Width/12, Height/50, Width/17, Width/17 );
 			btnNazad.Font = new Font( "", Width/50, FontStyle.Bold );
 		}
-	}
+        public void PravljenjeFunkcije(int i0,int u0,int w)
+        {
+            double u = u0 * Math.Sin(w * brojac);
+            double i = i0 * Math.Sin(w * brojac - brojac);
+            double uProslo = u0 * Math.Sin(w * (brojac - 1));
+            double iProslo = i0 * Math.Sin(w * (brojac - 1) - (brojac - 1));
+
+        }
+        private void pb1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void pb2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pb3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            brojac++;
+        }
+
+        private void pb1_Paint(object sender, PaintEventArgs e, double u, double i, double uProslo, double iProslo)
+        {
+            e.Graphics.DrawLine(olovka, brojac - 1,(float)uProslo, brojac, (float)u);
+            e.Graphics.DrawLine(olovka, brojac - 1, (float)iProslo, brojac, (float)i);
+        }
+
+        private void pb2_Paint(object sender, PaintEventArgs e, double u, double i, double uProslo, double iProslo)
+        {
+            e.Graphics.DrawLine(olovka, brojac - 1, (float)uProslo, brojac, (float)u);
+            e.Graphics.DrawLine(olovka, brojac - 1, (float)iProslo, brojac, (float)i);
+        }
+
+        private void pb3_Paint(object sender, PaintEventArgs e, double u, double i, double uProslo, double iProslo)
+        {
+            e.Graphics.DrawLine(olovka, brojac - 1, (float)uProslo, brojac, (float)u);
+            e.Graphics.DrawLine(olovka, brojac - 1, (float)iProslo, brojac, (float)i);
+        }
+    }
 }
