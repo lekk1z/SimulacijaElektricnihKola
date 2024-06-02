88571505fc8d59b32bdaf97f1f4e51936f7d93a6
AleksandraPopov <104566389+AleksandraPopov@users.noreply.github.com>
2024-06-02 16:09:38 +0200

    NAMESTENA TEORIJA SA SVE SLIKAMA, konacno

--- SimulacijaElektricnihKola/SimulacijaElektricnihKola/Teorija.cs
+++ SimulacijaElektricnihKola/SimulacijaElektricnihKola/Teorija.cs
@@ -39,16 +39,10 @@ namespace SimulacijaElektricnihKola
 		{
             this.Width = 1200;
             this.Height = 800;
-          
+            rtbTeorija.LoadFile("teorija.rtf");
             SetSize();
 
 		}
-        private void InsertImage(RichTextBox richTextBox, Image image)
-        {
-            Clipboard.SetImage(image);
-            richTextBox.Paste();
-            Clipboard.Clear();
-        }
         private void Teorija_ResizeEnd( object sender, EventArgs e )
 		{
             SetSize();
@@ -59,43 +53,7 @@ namespace SimulacijaElektricnihKola
             rtbTeorija.Font = new Font("", Width/70);
 			btnNazad.SetBounds( Width - Width/12, Height/50, Width/17, Width/17 );
 			btnNazad.Font = new Font( "", Width/50, FontStyle.Bold );
-            rtbTeorija.SelectionAlignment = HorizontalAlignment.Center;
-            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("JEDNOSMERNA STRUJA");
-            rtbTeorija.SelectionLength = "JEDNOSMERNA STRUJA".Length;
-             rtbTeorija.SelectionFont = new Font("",Width/50, FontStyle.Bold);
-            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("Primer:");
-            rtbTeorija.SelectionLength = "Primer:".Length;
-            rtbTeorija.SelectionFont = new Font(rtbTeorija.Font, FontStyle.Italic);
-            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("PRVI KIRHOFOV ZAKON");
-            rtbTeorija.SelectionLength = "PRVI KIRHOFOV ZAKON".Length;
-            rtbTeorija.SelectionFont = new Font("", Width / 50, FontStyle.Bold);
-            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("DRUGI KIRHOFOV ZAKON");
-            rtbTeorija.SelectionLength = "DRUGI KIRHOFOV ZAKON".Length;
-            rtbTeorija.SelectionFont = new Font("", Width / 50, FontStyle.Bold);
-            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("NAIZMENIČNA STRUJA");
-            rtbTeorija.SelectionLength = "NAIZMENIČNA STRUJA".Length;
-            rtbTeorija.SelectionFont = new Font("", Width / 50, FontStyle.Bold);
-            rtbTeorija.SelectionAlignment = HorizontalAlignment.Center;
-            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("Termogeni otpor (R)");
-            rtbTeorija.SelectionLength = "Termogeni otpor (R)".Length;
-            rtbTeorija.SelectionFont = new Font("", Width / 50, FontStyle.Bold);
-            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("Induktivni otpor (XL)");
-            rtbTeorija.SelectionLength = "Induktivni otpor (XL)".Length;
-            rtbTeorija.SelectionFont = new Font("", Width / 50, FontStyle.Bold);
-            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("Kapacitivni otpor (XC)");
-            rtbTeorija.SelectionLength = "Kapacitivni otpor (XC)".Length;
-            rtbTeorija.SelectionFont = new Font("", Width / 50, FontStyle.Bold);
-            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("Induktivna otpornost");
-            rtbTeorija.SelectionLength = "Induktivna otpornost".Length;
-            rtbTeorija.SelectionFont = new Font("", Width / 50, FontStyle.Bold);
-            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("Kapacitivna otpornost");
-            rtbTeorija.SelectionLength = "Kapacitivna otpornost".Length;
-            rtbTeorija.SelectionFont = new Font("", Width / 50, FontStyle.Bold);
-            rtbTeorija.SelectionStart = rtbTeorija.Text.IndexOf("Kapacitivna otpornost");
-            rtbTeorija.SelectionLength = "Kapacitivna otpornost".Length;
-            rtbTeorija.SelectionFont = new Font("", Width / 50, FontStyle.Bold);
-          //  Bitmap b = new Bitmap("C:\\Users\\PC\\Source\\Repos\\SimulacijaElektricnihKolaasdf\\SimulacijaElektricnihKola\\SimulacijaElektricnihKola\\slikaFaznoKasnjenjeNapona.jpg");
-          // InsertImage(rtbTeorija,b);
+            
         }
     }
 }
