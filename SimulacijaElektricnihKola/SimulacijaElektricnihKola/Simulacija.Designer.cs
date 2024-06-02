098d534e50fb823f626db074631c3c31a8b9ae76
lekk1z <2021.09.02@online.devetagimnazija.edu.rs>
2024-06-02 16:07:37 +0200

    simulacija dizajn

--- SimulacijaElektricnihKola/SimulacijaElektricnihKola/Simulacija.Designer.cs
+++ SimulacijaElektricnihKola/SimulacijaElektricnihKola/Simulacija.Designer.cs
@@ -29,9 +29,6 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.pb1 = new System.Windows.Forms.PictureBox();
-            this.pb2 = new System.Windows.Forms.PictureBox();
-            this.pb3 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.label5 = new System.Windows.Forms.Label();
@@ -46,45 +43,15 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.btnNazad = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pb1
-            // 
-            this.pb1.Location = new System.Drawing.Point(13, 281);
-            this.pb1.Margin = new System.Windows.Forms.Padding(4);
-            this.pb1.Name = "pb1";
-            this.pb1.Size = new System.Drawing.Size(176, 156);
-            this.pb1.TabIndex = 4;
-            this.pb1.TabStop = false;
-            this.pb1.Click += new System.EventHandler(this.pb1_Click);
-            // 
-            // pb2
-            // 
-            this.pb2.Location = new System.Drawing.Point(197, 281);
-            this.pb2.Margin = new System.Windows.Forms.Padding(4);
-            this.pb2.Name = "pb2";
-            this.pb2.Size = new System.Drawing.Size(176, 156);
-            this.pb2.TabIndex = 5;
-            this.pb2.TabStop = false;
-            this.pb2.Click += new System.EventHandler(this.pb2_Click);
-            // 
-            // pb3
-            // 
-            this.pb3.Location = new System.Drawing.Point(1, 148);
-            this.pb3.Margin = new System.Windows.Forms.Padding(4);
-            this.pb3.Name = "pb3";
-            this.pb3.Size = new System.Drawing.Size(500, 100);
-            this.pb3.TabIndex = 6;
-            this.pb3.TabStop = false;
-            this.pb3.Click += new System.EventHandler(this.pb3_Click);
-            this.pb3.Paint += new System.Windows.Forms.PaintEventHandler(this.pb3_Paint);
             // 
             // timer1
             // 
@@ -97,7 +64,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(505, 363);
+            this.label5.Location = new System.Drawing.Point(516, 363);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(57, 16);
             this.label5.TabIndex = 25;
@@ -105,7 +72,7 @@
             // 
             // trackBar5
             // 
-            this.trackBar5.Location = new System.Drawing.Point(508, 382);
+            this.trackBar5.Location = new System.Drawing.Point(519, 382);
             this.trackBar5.Name = "trackBar5";
             this.trackBar5.Size = new System.Drawing.Size(280, 56);
             this.trackBar5.TabIndex = 24;
@@ -113,7 +80,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(505, 304);
+            this.label4.Location = new System.Drawing.Point(516, 304);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(107, 16);
             this.label4.TabIndex = 23;
@@ -121,7 +88,7 @@
             // 
             // trackBar4
             // 
-            this.trackBar4.Location = new System.Drawing.Point(508, 323);
+            this.trackBar4.Location = new System.Drawing.Point(519, 323);
             this.trackBar4.Name = "trackBar4";
             this.trackBar4.Size = new System.Drawing.Size(280, 56);
             this.trackBar4.TabIndex = 22;
@@ -129,7 +96,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(505, 242);
+            this.label3.Location = new System.Drawing.Point(516, 242);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(82, 16);
             this.label3.TabIndex = 21;
@@ -137,7 +104,7 @@
             // 
             // trackBar3
             // 
-            this.trackBar3.Location = new System.Drawing.Point(508, 261);
+            this.trackBar3.Location = new System.Drawing.Point(519, 261);
             this.trackBar3.Name = "trackBar3";
             this.trackBar3.Size = new System.Drawing.Size(280, 56);
             this.trackBar3.TabIndex = 20;
@@ -145,7 +112,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(505, 148);
+            this.label2.Location = new System.Drawing.Point(516, 148);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(85, 16);
             this.label2.TabIndex = 19;
@@ -153,7 +120,7 @@
             // 
             // trackBar1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(508, 167);
+            this.trackBar1.Location = new System.Drawing.Point(519, 167);
             this.trackBar1.Maximum = 20;
             this.trackBar1.Name = "trackBar1";
             this.trackBar1.Size = new System.Drawing.Size(280, 56);
@@ -161,7 +128,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(508, 81);
+            this.button3.Location = new System.Drawing.Point(519, 81);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(137, 23);
             this.button3.TabIndex = 28;
@@ -171,7 +138,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(651, 12);
+            this.button2.Location = new System.Drawing.Point(662, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(137, 50);
             this.button2.TabIndex = 27;
@@ -181,7 +148,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(508, 12);
+            this.button1.Location = new System.Drawing.Point(519, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(137, 50);
             this.button1.TabIndex = 26;
@@ -191,7 +158,7 @@
             // 
             // btnNazad
             // 
-            this.btnNazad.Location = new System.Drawing.Point(696, 12);
+            this.btnNazad.Location = new System.Drawing.Point(707, 12);
             this.btnNazad.Name = "btnNazad";
             this.btnNazad.Size = new System.Drawing.Size(50, 52);
             this.btnNazad.TabIndex = 2;
@@ -199,11 +166,31 @@
             this.btnNazad.UseVisualStyleBackColor = true;
             this.btnNazad.Click += new System.EventHandler(this.btnNazad_Click);
             // 
+            // pb3
+            // 
+            this.pb3.Location = new System.Drawing.Point(12, 324);
+            this.pb3.Margin = new System.Windows.Forms.Padding(4);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(500, 100);
+            this.pb3.TabIndex = 6;
+            this.pb3.TabStop = false;
+            this.pb3.Click += new System.EventHandler(this.pb3_Click);
+            this.pb3.Paint += new System.Windows.Forms.PaintEventHandler(this.pb3_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 305);
+            this.pictureBox1.TabIndex = 29;
+            this.pictureBox1.TabStop = false;
+            // 
             // Simulacija
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(862, 450);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -216,8 +203,6 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.pb3);
-            this.Controls.Add(this.pb2);
-            this.Controls.Add(this.pb1);
             this.Controls.Add(this.btnNazad);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Simulacija";
@@ -225,13 +210,12 @@
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Simulacija_FormClosed);
             this.Load += new System.EventHandler(this.Simulacija_Load);
             this.ResizeEnd += new System.EventHandler(this.Simulacija_ResizeEnd);
-            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -239,9 +223,6 @@
 
         #endregion
         private System.Windows.Forms.Button btnNazad;
-        private System.Windows.Forms.PictureBox pb1;
-        private System.Windows.Forms.PictureBox pb2;
-        private System.Windows.Forms.PictureBox pb3;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Label label5;
@@ -255,5 +236,7 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
